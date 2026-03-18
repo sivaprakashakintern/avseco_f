8dff7983557d9928fabacc9d7e91edc2677434fc
jofra-shiva <shivaprakash3115@gmail.com>
2026-03-18 23:13:26 +0530

    feat: Implement ProductList page with product management functionalities including CRUD operations, filtering, and search.

--- src/pages/products/ProductList.js
+++ src/pages/products/ProductList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./ProductList.css";
 import { productsApi } from "../../utils/api.js";
 import { useAppContext } from "../../context/AppContext.js";
